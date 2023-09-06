8b31f1620dd7691f946fea526e0ce45178367dc6
k7roman <kaizen196@gmail.com>
2023-09-06 06:05:32 +0300

    Implemented the UI and fetch functionality

--- src/app/components/view/view.component.ts
+++ src/app/components/view/view.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Apollo } from 'apollo-angular';
+import { GET_COUNTRIES } from 'src/app/graphql.queries/graphql.countries.queries';
 
 @Component({
   selector: 'app-view',
@@ -9,6 +11,23 @@ import { RouterLink } from '@angular/router';
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css']
 })
-export class ViewComponent {
+export class ViewComponent implements OnInit {
+  countries: any[] = [];
+  error: any;
+  loading : boolean = true;
 
+  constructor(private apollo : Apollo){}
+
+  ngOnInit(): void {
+    this.loadCountries();
+  }
+
+  loadCountries(){  
+    this.apollo.watchQuery({
+      query: GET_COUNTRIES
+    }).valueChanges.subscribe(({data, error} : any) => {
+      this.loading = false;
+      this.countries = data.countries;
+    })
+  }
 }
