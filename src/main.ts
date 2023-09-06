6c91afb6348afafb32a0ed317929d382d9bc1b92
k7roman <kaizen196@gmail.com>
2023-09-06 06:04:35 +0300

    Added apollo config

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,32 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { provideRouter } from "@angular/router";
 import { routes } from "./app/app-routing.module";
+import { Apollo, APOLLO_OPTIONS } from "apollo-angular";
+import { HttpLink } from 'apollo-angular/http';
+import { ApolloClientOptions, ApolloLink, InMemoryCache } from '@apollo/client/core';
+import { myUrl } from "./app/graphql.config";
+import { importProvidersFrom } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 bootstrapApplication(
-  AppComponent,
+  AppComponent, 
   {
-    providers: [provideRouter(routes)]
+    providers: [
+      importProvidersFrom(HttpClientModule),
+      provideRouter(routes),
+      {
+        provide: APOLLO_OPTIONS,
+        useFactory: (
+          httpLink: HttpLink,
+        ): ApolloClientOptions<unknown> => ({
+          link: ApolloLink.from([
+            httpLink.create({ uri: myUrl }),
+          ]),
+          cache: new InMemoryCache(),
+        }),
+        deps: [HttpLink],
+      },
+      Apollo,
+    ],
   }
 );
